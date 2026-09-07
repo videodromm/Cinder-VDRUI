0317a905e097a10748f2a615dfb10d433ca80f42
Bruce LANE <b.lane@agglo-casa.fr>
2026-09-07 15:37:03 +0200

    info.v002.syphon

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 echo Install dependencies
 echo Cinder-Spout
 git clone https://github.com/brucelane/Cinder-Spout ../Cinder-Spout
+echo Cinder-Syphon
+git clone https://github.com/naychrist/Cinder-Syphon ../Cinder-Syphon
 echo Required Cinder blocks checked out
